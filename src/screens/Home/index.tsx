6ca0f7147455596fbcd79b6b1acbaa8d1d57e309
Luiz Henrique <luizhjramos@outlook.com>
2024-01-05 21:45:33 -0300

    refactor: add screen layer structure

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text style={{ color: "#fff" }}>Hello World!</Text>
+  </View>
+);
